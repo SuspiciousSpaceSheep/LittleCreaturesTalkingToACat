ae086c8a10cbe8dbe6d60b62270907cf5991d527
nebulark <florian.schmiderer@posteo.net>
2026-01-14 11:21:35 +0100

    basic 3d camera, outline shader, wip outline on hover

---
+++ littlecreaturestalkingtoacat/Scripts/CameraMovement3D.gd
@@ -0,0 +1,34 @@
+extends Node
+
+@export var speed : float = 5.0
+@export var edge_fraction: float = 0.1
+
+@onready var camera : Camera3D  = get_node(".") as Camera3D
+
+	
+func _process(_delta: float):
+	var vp = get_viewport();
+
+	
+	var x_pos_fraction = vp.get_mouse_position().x / vp.size.x;
+	
+	if x_pos_fraction < edge_fraction:
+		camera.h_offset -= speed * _delta;
+	elif x_pos_fraction > (1-edge_fraction):
+		camera.h_offset += speed * _delta;
+		
+	#var transform : Transform3D = camera.get_camera_transform();
+	#if mousePos.x < 0:
+		#camera.get_camera_transform() -= speed * _delta;
+	#elif mousePos.x > 0:
+		#camera.h_offset += speed * _delta;
+	
+	
+	
+	#if mousePos.x < get_viewport().size.x / edge_percent:
+		#camera.h_offset -= speed * _delta;
+	#elif mousePos.x > get_viewport().size.x - get_viewport().size.x / edge_percent:
+		#camera.h_offset += speed * _delta;
+		
+		
+	
---
+++ littlecreaturestalkingtoacat/Scripts/outline_hover.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@onready var mesh : MeshInstance3D  = get_node(".") as MeshInstance3D
+var outline_color_name : StringName = "outline_color";
+var outline_material : ShaderMaterial;
+var is_hightlighted : bool = false;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#mesh.material.set_shader_parameter(outline_color_name, Vector4(0.0, 1.0, 0.0, 0.0))
+	print(mesh);
+	print(mesh.get_active_material(0));
+	var mat = mesh.get_active_material(0);
+	outline_material = mat.next_pass as ShaderMaterial;
+	set_hightlighted(false);
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func set_hightlighted(new_is_highlighted):
+	is_hightlighted = new_is_highlighted
+	if (new_is_highlighted):
+		outline_material.set_shader_parameter(outline_color_name, Vector4(0.0, 1.0, 0.0, 1.0))
+	else:
+		outline_material.set_shader_parameter(outline_color_name, Vector4(0.0, 1.0, 0.0, 0.0))
+
+# TODO: Add a global script that makes a mouse raycast, setting the objects to hightled or not
