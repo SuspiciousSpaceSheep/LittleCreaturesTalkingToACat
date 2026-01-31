d52747fe2a4030f0676c2ce76846efa116137a78
SpaceSheep <lena.lamberger@gmx.at>
2026-01-31 13:22:21 +0100

    wip adding second bunny

--- littlecreaturestalkingtoacat/Assets/bunny.gd
+++ littlecreaturestalkingtoacat/Assets/bunny.gd
@@ -4,6 +4,8 @@ extends Node3D
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
+			print("bny klicked");
+			print(get_node("."));
 			Dialogic.start("Bunny_says_hi")
 			
 		
